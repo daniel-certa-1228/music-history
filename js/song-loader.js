e7dffc75c94a56ac0f7ea0295e2366b368d23c4b
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-09 09:33:19 -0500

    partially converted to jQuery

--- js/song-loader.js
+++ js/song-loader.js
@@ -1,54 +1,60 @@
 console.log( "songs.js");
 //Define ES6 IIFE
+
 {
 	var SongSpace = {};
 }
+
+$(document).ready(function(){
+	
+
 //IIFE that contains initial song-loader function
-{
+	{
 
-	SongSpace.loadSongInfo = () => {
+		SongSpace.loadSongInfo = () => {
 
-		let songObject = [];
+			let songObject = [];
 
-		let songloader = new XMLHttpRequest();
-		songloader.addEventListener("load", songLoadComplete);
+			$.ajax({
+				url:"../json/music.json"
+			}).done(songLoadComplete)
+			  .fail(function(error) {
+			  	alert("error")
+			  })
 
-		function songLoadComplete(event) {
-			console.log( "songload complete" );
-			songObject = JSON.parse(event.target.responseText);
-			// console.log( "songObject", songObject );
-			SongSpace.passJSON1(songObject);
-			let songNumber = songObject.length;
-			SongSpace.passDefaultValue(songNumber)
+			function songLoadComplete(json) {
+				console.log( json );
+				songObject = json;
+				SongSpace.passJSON1(songObject);
+				let songNumber = songObject.length;
+				SongSpace.passDefaultValue(songNumber)
+			}
+		};
+
+		SongSpace.loadSongInfo();
+	}
+	//IIFE that contains the second song-loader function
+	{
+		SongSpace.loadMoreSongs = () => {
+
+			let moreSongsObject = [];
+			let moreSongsLoader = new XMLHttpRequest();
+			moreSongsLoader.addEventListener("load", moreSongsComplete);
+			moreSongsLoader.open("GET", "../json/music-2.json")
+			moreSongsLoader.send();
+
+			function moreSongsComplete(event) {
+				console.log( "More songs loaded.");
+
+				moreSongsObject = JSON.parse(event.target.responseText);
+				// console.log( "moreSongsObject", moreSongsObject );
+				SongSpace.passJSON2(moreSongsObject);
+
+			}
+
+
 		}
-
-		songloader.open("GET", "../json/music.json");
-		songloader.send();
-
-	};
-
-	SongSpace.loadSongInfo();
-}
-//IIFE that contains the second song-loader function
-{
-	SongSpace.loadMoreSongs = () => {
-
-		let moreSongsObject = [];
-		let moreSongsLoader = new XMLHttpRequest();
-		moreSongsLoader.addEventListener("load", moreSongsComplete);
-		moreSongsLoader.open("GET", "../json/music-2.json")
-		moreSongsLoader.send();
-
-		function moreSongsComplete(event) {
-			console.log( "More songs loaded.");
-
-			moreSongsObject = JSON.parse(event.target.responseText);
-			// console.log( "moreSongsObject", moreSongsObject );
-			SongSpace.passJSON2(moreSongsObject);
-
-		}
-
 
 	}
 
-}
+})  //End DOCUMENT READY
